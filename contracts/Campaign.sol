891ada570d8097f94916b13055da3518cb022746
jhinds <jonathan.a.hinds@gmailc.om>
2017-10-19 12:03:05 +0400

    added campagin

---
+++ contracts/Campaign.sol
@@ -0,0 +1,52 @@
+pragma solidity 0.4.15;
+
+import "./Owned.sol";
+
+contract Campaign is Owned {
+
+  mapping(address => bool) donatorRegistry;
+  uint currentBalance;
+  uint goalAmount;
+  bytes32 ipfsHash;
+  mapping(address => uint) donations;
+
+
+  event LogDonation(address sender, uint amount);
+  event LogWithdrawl(address sender, uint amount);
+  event LogPaused(address sender);
+  event LogFundsTransfered(address sender, uint amount);
+
+  function Campaign(bytes32 _ipfsHash, uint _goalAmount) {
+    // TODO support list of owners
+    ipfsHash = _ipfsHash;
+    goalAmount = _goalAmount;
+  }
+
+  /*modifier rateLimit();*/
+
+  function returnFunds() {
+    require(currentBalance > goalAmount);
+    msg.sender.transfer(donations[msg.sender]);
+  }
+
+  function getBalance() constant returns (uint) {
+    return currentBalance;
+  }
+
+  function donate() payable returns (bool) {
+    donations[msg.sender] += msg.value;
+    return true;
+  }
+
+  function transferFunds(address supplier, uint amount)
+           onlyOwner {
+             currentBalance - amount;
+             supplier.transfer(amount);
+           }
+
+  function setNewIpfs(bytes32 newIpfsHash)
+    onlyOwner {
+    ipfsHash = newIpfsHash;
+  }
+
+}
---
+++ contracts/Owned.sol
@@ -0,0 +1,24 @@
+pragma solidity 0.4.15;
+
+contract Owned {
+
+    address public owner;
+
+    event LogNewOwner(address sender, address oldOwner, address newOwner);
+
+    function Owned() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender != owner);
+        _;
+    }
+
+    function changeOwner(address newOwner) onlyOwner returns (bool success) {
+        require(newOwner == 0);
+        LogNewOwner(msg.sender, owner, newOwner);
+        owner = newOwner;
+        return true;
+    }
+}
