92946db2ff7f8ea5dbfbba27ee407eb68fd9e74d
jhinds <jonathan.a.hinds@gmailc.om>
2017-10-19 17:29:29 +0400

    another one

--- test/vendors.js
+++ test/vendors.js
@@ -27,14 +27,22 @@ contract('Vendors', (accounts) => {
     const txAddHash = await vendorContract.addVendor(verifiedSupplier, ipfsHash, { from: venderOwner });
     assert.equal(txAddHash.logs[0].event, "LogVendorAdded");
     assert.equal(txAddHash.logs[0].args.ipfsHash, ipfsHash);
+    const txAddVendorTag = await vendorContract.addVendorTag(verifiedSupplier, lumberHex);
+    assert.equal(txAddVendorTag.logs[0].event, "LogVendorTagAdded");
+    assert.equal(txAddVendorTag.logs[0].args.tag, lumberHex);
+    assert.equal(txAddVendorTag.logs[0].args.vendorAddr, verifiedSupplier);
   });
 
   it("Should be able to add vendors", async () => {
+    const lumberHex = "0x6c756d6265720000000000000000000000000000000000000000000000000000";
+    const ipfsHash = "0x6861736800000000000000000000000000000000000000000000000000000000";
+    const txAddTag = await vendorContract.addTag("lumber", { from: venderOwner });
+    const txAddHash = await vendorContract.addVendor(verifiedSupplier, ipfsHash, { from: venderOwner });
+    assert.equal(txAddHash.logs[0].event, "LogVendorAdded");
+    assert.equal(txAddHash.logs[0].args.ipfsHash, ipfsHash);
   });
 
   it("Should not be able to add a tag to a vendor that does not exist", async () => {
   });
 
-  it("Should able to create a new campaign", async () => {
-  });
 });
