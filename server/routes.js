abc124462a2b4869c964785cc9a7d77184ee61a1
bryn austin bellomy <bryn.bellomy@gmail.com>
2017-10-19 14:15:17 +0400

    Adding node.js backend

---
+++ server/routes.js
@@ -0,0 +1,11 @@
+
+const router = require('express').Router()
+
+const asyncMiddleware = fn =>
+    (req, res, next) => { Promise.resolve(fn(req, res, next)).catch(next) }
+
+router.get('/test', asyncMiddleware(async (req, res, next) => {
+    res.json({ success: true })
+}))
+
+module.exports = router
