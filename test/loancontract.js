5c9faefe432b323e9bc7f6782d3ab6958222a081
Sangphil Kim <sangphilkim@aol.com>
2017-10-20 17:33:56 +0400

    prepare loancontract test

---
+++ test/loancontract.js
@@ -0,0 +1,26 @@
+/*
+const LoanContract = artifacts.require("./LoanContract.sol");
+const { expectThrow } = require("./LoanContract.js");
+
+contract('Loan', (accounts) => {
+
+  let loanContract;
+  const loanOwner = accounts[0];
+
+  beforeEach(() => {
+    return LoanContract.new({from: loanOwner })
+    .then(instance => {
+      loanContract = instance
+    });
+  });
+
+  it("Should be able to add a tag", async () => {
+
+  });
+
+  it("Should be able to add a tag to a vendor", async () => {
+
+  });
+});
+
+*/
