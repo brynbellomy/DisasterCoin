15d23dbd2d3c47f05c97f7619a75c85f4a32b0ca
jhinds <jonathan.a.hinds@gmailc.om>
2017-10-21 09:58:59 +0400

    working nginx

--- deploy/start.sh
+++ deploy/start.sh
@@ -7,11 +7,10 @@ testrpc &
 cd /opt && truffle deploy
 
 # build frontend
-# cd /opt/frontend
-# yarn install
-# cp -r /opt/build/contracts /opt/frontend/node_modules/contracts
-# yarn run build
-# cp -r /opt/frontend/build /opt/frontend_static
+cd /opt/frontend && yarn install
+cp -r /opt/build/contracts /opt/frontend/node_modules/contracts
+cd /opt/frontend && yarn run build
+cp -r /opt/frontend/build /opt/frontend_static
 
 
 # start backend
