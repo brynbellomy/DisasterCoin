ea183107dff66d1544be2d0c490cfdeb7161b532
jhinds <jonathan.a.hinds@gmailc.om>
2017-10-21 10:36:53 +0400

    copying campagin page

--- deploy/start.sh
+++ deploy/start.sh
@@ -9,6 +9,7 @@ cd /opt && truffle deploy
 # build frontend
 cd /opt/frontend && yarn install
 cp -r /opt/build/contracts /opt/frontend/node_modules/contracts
+cp /opt/frontend/src/components/campaignPage.js /opt/frontend/src/components/CampaignPage.js
 cd /opt/frontend && yarn run build
 cp -r /opt/frontend/build /opt/frontend_static
 
